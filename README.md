669aea7c3ea90d07931deb6cbb800777909163d0
Rodrigo Silva <rodrigo-wow@hotmail.com>
2022-01-29 02:30:06 -0300

    Projeto jogo Jokenpo

---
+++ README.md
@@ -0,0 +1,64 @@
+# <strong><p align="center">README</p></strong>
+
+<p align="center">Esse projeto tem como foco em fazer um jogo jokenpô, é um jogo de mãos recreativo e simples para você jogar contra o computador. O jogo é frequentemente empregado como método de seleção, assim como lançar moedas, jogar dados, entre outros.</p>
+
+<br>
+
+<h1 align="center">Tabela de conteúdo</h1>
+<p align="center">
+<a href="#sobre">Sobre</a> -
+<a href="#roadmap">Roadmap</a> -
+<a href="#tecnologias">Tecnologias</a> -
+<a href="#pré-requisitos">Pré Requisitos</a>
+</p>
+
+# <h2>Sobre</h2>
+<p>O jogo é conhecido em vários lugares do mundo, e é acompanhado de "gritos" ou rimas diferentes: Nos países lusófonos, os jogadores geralmente dizem "Pedra, Papel, Tesoura" antes de jogar. Em algumas regiões do Brasil usa-se "Janken-pon" devido à influência de imigrantes japoneses, sendo que pedra, papel e tesoura não é o nome usual do jogo em São Paulo.<br><h3>Regras do jogo:</h3> Pedra ganha da tesoura (amassando-a ou quebrando-a).<br>Tesoura ganha do papel (cortando-o).<br>Papel ganha da pedra (embrulhando-a).</p>
+
+<br>
+
+# <h2>Roadmap</h2>
+<h1>
+  <img alt="Readme" title="Readme" src="./assets/img/gif_01.gif">
+  <img alt="Readme" title="Readme" src="./assets/img/gif_02.gif">
+  <img alt="Readme" title="Readme" src="./assets/img/gif_03.gif">
+  <img alt="Readme" title="Readme" src="./assets/img/gif_04.gif">
+</h1>
+<br>
+
+# <h2>🦾👾 Tecnologias</h2>
+
+<p>As seguintes ferramentas foram usadas na construção do projeto:</p>
+
+- [HTML](https://developer.mozilla.org/pt-BR/docs/Web/HTML)
+- [CSS](https://developer.mozilla.org/pt-BR/docs/Web/CSS)
+- [JAVASCRIPT](https://developer.mozilla.org/pt-BR/docs/Web/JavaScript)
+
+<br>
+
+# <h2>Pré-requisitos</h2>
+
+Antes de começar, você vai precisar ter instalado em sua máquina as seguintes ferramentas: [GIT](https://git-scm.com/downloads), [NODE.JS](https://nodejs.org/en/download/). Além disto é bom ter um editor para trabalhar ou visualizar o código como [VSCODE](https://code.visualstudio.com/download).
+
+### 🤖 Rodando o Front End
+
+```bash
+## Clone este repositório
+$ git clone https://github.com/RodrigoSilva95/Jokenpo
+
+## Acesse a pasta do projeto no terminal/cmd
+$ cd Jokenpo
+
+## Inicialize a sua IDE de preferência VSCODE.
+$ code .
+
+## Execute a aplicação em modo desenvolvimento e abra o index.html
+$ CTRL + SHIFT + V
+
+## O servidor iniciará na porta: 8080 - acesse <https://localhost:8080>
+$ Por fim, execute e teste aplicação.
+```
+<hr>
+<br>
+
+😁👍 [Siga-me no LinkedIn](https://www.linkedin.com/in/rodrigo-de-lima-santos-silva/)
--- assets/logica.js
+++ assets/logica.js
@@ -1,139 +1,134 @@
-// Codigo JS do jogo
-let jogadorNome = prompt("Qual é o seu nome?")
-let definirJogadorNome = (jogadorNome)
+var jogadorNome;
+var jogadorPontos = 0;
+var jogadorEscolha = 0;
 
-let mensagem = ('Bem-vindo ' + jogadorNome + ' está preparado? Escolha uma opção acima...')
-let jogadorPontos = 0
-let jogadorEscolha = 0
+var computadorEscolha = 0;
+var computadorPontos = 0;
 
-let computadorEscolha = 0
-let computadorPontos = 0
-
-/* 
-    Gera os números randomicos
-    1 - Pedra 
-    2 - Papel
-    3 - Tesoura
-*/ 
-function sortear(min, max){
-    return math.floor(math.random() * (max - min + 1)) + min;
+// Gera números randomicos.
+// 1 - Pedra
+// 2 - Papel
+// 3 - Tesoura
+function sortear(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-/* 
-    Traduz o número em frase 
-    1 - Pedra 
-    2 - Papel
-    3 - Tesoura
-*/
-function traduzirEscolha(numero){
-    if(numero == 1){
-        return 'Pedra';
-    }
-
-    else if(numero == 2){
-        return 'Papel';
-    }
-    else if(numero == 3){
-        return 'Tesoura';
-    }
+// Traduz o número em frase.
+// 1 - Pedra
+// 2 - Papel
+// 3 - Tesoura
+function traduzirEscolha(numero) {
+  if(numero == 1) {
+    return 'Pedra';
+  }
+  else if(numero == 2) {
+    return 'Papel';
+  }
+  else if(numero == 3) {
+    return 'Tesoura';
+  }
 }
 
-// Adiciona a classe selecionada
-function selecionar(tipo, escolha){
-    document.getElementById(tipo + '-escolha-' + escolha).classList.add('selecionado');
+// Adiciona a classe selecionado.
+function selecionar(tipo, escolha) {
+  document.getElementById(tipo + '-escolha-' + escolha).classList.add('selecionado');
 }
 
-// Remove a classe selecionada
-function deselecionar(tipo, escolha){
-    document.getElementById(tipo + '-escolha-' + escolha).classList.remove('selecionado');
+// Remove a classe selecionado.
+function deselecionar(tipo, escolha) {
+  document.getElementById(tipo + '-escolha-' + escolha).classList.remove('selecionado');
 }
 
 // Escreve na tela uma mensagem.
 function mensagem(texto){
-    document.getElementById('mensagem').innerHTML = texto;
+  document.getElementById('mensagem').innerHTML = texto;
 }
 
 // Escreve no placar o nome do jogador.
-function definirJogadorNome(nome){
-    document.getElementById('jogador-nome').innerHTML = nome;
+function definirJogadorNome(nome) {
+  document.getElementById('jogador-nome').innerHTML = nome;
 }
 
 // Calcula e retorna quem ganhou.
 // 0 - Empate
 // 1 - Jogador
 // 2 - Computador
-function calcularEscolha(jogador, computador){
-    if (jogador == 1 && computador == 1){
+function calcularEscolha(jogador, computador) {
+  if (jogador == 1 && computador == 1) {
     return 0;
-    }
-    else if (jogador == 1 && computador == 2){
+  }
+  else if (jogador == 1 && computador == 2) {
     return 2;
-    }
-    else if (jogador == 1 && computador == 3){
+  }
+  else if (jogador == 1 && computador == 3) {
     return 1;
-    }
-    else if (jogador == 2 && computador == 1){
+  }
+  else if (jogador == 2 && computador == 1) {
     return 1;
-    }
-    else if (jogador == 2 && computador == 2){
+  }
+  else if (jogador == 2 && computador == 2) {
     return 0;
-    }
-    else if (jogador == 2 && computador == 3){
+  }
+  else if (jogador == 2 && computador == 3) {
     return 2;
-    }
-    else if (jogador == 3 && computador == 1){
+  }
+  else if (jogador == 3 && computador == 1) {
     return 2;
-    }
-    else if (jogador == 3 && computador == 2){
+  }
+  else if (jogador == 3 && computador == 2) {
     return 1;
-    }
-    else if (jogador == 3 && computador == 3){
+  }
+  else if (jogador == 3 && computador == 3) {
     return 0;
-    }
+  }
 }
 
 // Soma os pontos do jogador
-function somarPontoJogador(){
-    jogadorPontos++;
-    document.getElementById('jogador-pontos').innerHTML = jogadorPontos;
+function somarPontoJogador() {
+  jogadorPontos++;
+  document.getElementById('jogador-pontos').innerHTML = jogadorPontos;
 }
 
 // Soma os pontos do computador
-function somarPontoComputador(){
-    computadorPontos++;
-    document.getElementById('computador-pontos').innerHTML = computadorPontos;
+function somarPontoComputador() {
+  computadorPontos++;
+  document.getElementById('computador-pontos').innerHTML = computadorPontos;
 }
 
 // Função de jogar
-function jogar(escolha){
-    jogadorEscolha = escolha;
-    selecionar('jogador', jogadorEscolha);
+function jogar(escolha) {
+  jogadorEscolha = escolha;
+  selecionar('jogador', jogadorEscolha);
 
-    computadorEscolha = sortear(1, 3);
-    selecionar('computador', computadorEscolha);
+  computadorEscolha = sortear(1, 3);
+  selecionar('computador', computadorEscolha);
 
-    var ganhador = calcularEscolha(jogadorEscolha, computadorEscolha);
+  var ganhador = calcularEscolha(jogadorEscolha, computadorEscolha);
 
-    if (ganhador == 0){
+  if (ganhador == 0) {
     mensagem('Empate');
-    }
-    else if (ganhador == 1){
+  }
+  else if (ganhador == 1) {
     mensagem('Ponto para ' + jogadorNome);
     somarPontoJogador();
-    }
-    else if (ganhador == 2){
+  }
+  else if (ganhador == 2) {
     mensagem('Ponto para Computador');
     somarPontoComputador();
-    }
+  }
 
-    setTimeout(function(){
+  setTimeout(function() {
     deselecionar('jogador', jogadorEscolha);
     deselecionar('computador', computadorEscolha);
     mensagem(jogadorNome + ' escolha uma opção...');
-    }, 3500);
+  }, 3500);
 }
 
 document.getElementById('jogador-escolha-1').onclick = function(){ jogar(1); };
 document.getElementById('jogador-escolha-2').onclick = function(){ jogar(2); };
 document.getElementById('jogador-escolha-3').onclick = function(){ jogar(3); };
 
+jogadorNome = prompt("Qual é o seu nome?");
+definirJogadorNome(jogadorNome);
+
+mensagem('Bem-vindo ' + jogadorNome + ' está preparado? Escolha uma opção acima...');
